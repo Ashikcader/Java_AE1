df29ac22e8e82454b2804890dc3e24ef82f8ff40
Moahmmed Ashik <148471336+Ashikcader@users.noreply.github.com>
2024-11-18 19:52:01 +0000

    Update Book.java

--- src/main/java/com/mycompany/mavenproject1/Book.java
+++ src/main/java/com/mycompany/mavenproject1/Book.java
@@ -8,5 +8,40 @@
  * @author ashik
  */
 public class Book {
-    
+    private String title;
+    private String author;
+    private double price;
+    private int sales;
+
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.sales = 0;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getSales() {
+        return sales;
+    }
+
+    public void incrementSales() {
+        this.sales++;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Author: " + author + ", Price: $" + price;
+    }
 }
