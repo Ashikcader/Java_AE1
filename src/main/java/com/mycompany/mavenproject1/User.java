ff1c1afc99db2cafc5b2e594e6ddf23d79f65ab3
QfoxyQ <148481653+QfoxyQ@users.noreply.github.com>
2024-11-19 11:16:38 +0000

    Update User.java

--- src/main/java/com/mycompany/mavenproject1/User.java
+++ src/main/java/com/mycompany/mavenproject1/User.java
@@ -14,14 +14,21 @@ public class User {
     private final String username;
     private String password; //Made mutable to allow password updates
     private final String role; //Changed from boolean to string for more flexibility (e.g., "admin", "user")
-    private final List<Book> orderHistory;
+    private final List<Book> orderHistory = new ArrayList<>();
 
     // Constructor
     public User(String username, String password, String role) {
         this.username = username;
         this.password = password;
         this.role = role;
-        this.orderHistory = new ArrayList<>();
     }
+    
+    // Add Order to History
+    public void addOrder(Order order){
+        if (order !=null) {
+            orderHistory.add(order);
+        }
+    }
+    
         
 }
